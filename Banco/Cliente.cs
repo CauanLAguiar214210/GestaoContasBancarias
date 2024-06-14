1d08b966bda8d5d6a28c79fa16985f5f10e0d1d9
CauanLAguiar <132419025+CauanLAguiar214210@users.noreply.github.com>
2024-06-14 14:29:48 -0300

    commit1
    Analisar novamente para melhorias e refaturar para WebProject

---
+++ Banco/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Banco
+{
+    internal class Cliente
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Cliente(int numeroConta, string nome) 
+        {
+            NumeroConta = numeroConta;
+            Nome = nome;
+        }
+
+        public Cliente(int numeroConta, string nome, double saldo) : this(numeroConta, nome)
+        {
+            Saldo = saldo;
+        }
+                      
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+
+        }
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+
+        }
+               
+        public override string ToString()
+        {
+            return "Conta: " 
+                + NumeroConta 
+                + ", Titular: " 
+                + Nome 
+                + ", Saldo: R$" 
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente conta;
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Cliente(numero, nome, depositoInicial);
+            }
+            else
+            {
+                conta = new Cliente(numero, nome);
+            }
+
+            Console.WriteLine("\nDados da conta:\n" + conta);
+
+            Console.Write("\nEntre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("\nEntre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
